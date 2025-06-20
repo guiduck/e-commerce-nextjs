2dfd58a9881e1ef03d7534dacf8977098c192683
guilherme <guiduck02@gmail.com>
2025-06-20 15:25:01 -0300

    fix: cypress ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
     supportFile: "cypress/support/e2e.ts",
     specPattern: "src/components/**/*.cy.{js,ts,jsx,tsx}",
+    defaultCommandTimeout: 8000,
+    pageLoadTimeout: 60000,
     setupNodeEvents(on, config) {
       return config;
     },
